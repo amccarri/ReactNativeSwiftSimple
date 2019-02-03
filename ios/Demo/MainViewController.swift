7280c0e83bb9b17171bf0db457aa2f36a2a79cbf
Alex McCarrier <amccarrier@atlassian.com>
2019-02-03 16:11:42 -0600

    Added cocoapods and some animated Rx stuff.

--- ios/Demo/MainViewController.swift
+++ ios/Demo/MainViewController.swift
@@ -7,38 +7,59 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxAnimated
 
 class MainViewController: UIViewController {
-    
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    let reactView: UIView = {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return UIView()
-        }
-        
-        return appDelegate.reactViewController.view
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupReactView()
+  
+  @IBOutlet weak var rxOutputLabel: UILabel!
+  @IBOutlet weak var titleLabel: UILabel!
+  
+  private let message = BehaviorRelay(value: "Loading...")
+  
+  private let disposeBag = DisposeBag()
+  
+  let reactView: UIView = {
+    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+      return UIView()
     }
     
-    private func setupReactView() {
-        reactView.translatesAutoresizingMaskIntoConstraints = false
-        reactView.backgroundColor = UIColor.lightGray
-        
-        view.addSubview(reactView)
+    return appDelegate.reactViewController.view
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupReactView()
 
-        let margins = view.layoutMarginsGuide
-        NSLayoutConstraint.activate([
-            reactView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
-            reactView.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 8),
-            reactView.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -8),
-            reactView.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -8)
-        ])
-    }
+    message.asDriver()
+      .bind(animated: rxOutputLabel.rx.animated.flip(.top, duration: 2).text)
+      .disposed(by: disposeBag)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    Observable<Int>.timer(1.0, scheduler: MainScheduler.instance)
+      .debug()
+      .subscribe(onNext: { _ in
+        self.message.accept("Loaded.")
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  private func setupReactView() {
+    reactView.translatesAutoresizingMaskIntoConstraints = false
+    reactView.backgroundColor = UIColor.lightGray
+    
+    view.addSubview(reactView)
+    
+    let margins = view.layoutMarginsGuide
+    NSLayoutConstraint.activate([
+      reactView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+      reactView.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 8),
+      reactView.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -8),
+      reactView.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -44)
+      ])
+  }
 }
 
